2504e751f9904851dd7c94aea1b96d761d3ffc11
William Buchanan <william@williambuchanan.net>
2016-11-21 09:50:34 -0500

    Added placeholders for data elements identified by Daphne for the demographics query and also changed the final logic back to testing whether or not the end date is greater than or equal to the start date.  Added query script used to geocode the addresses for redistricting purposes.  Requires PostgreSQL 9.5 or later along with the following extensions: postgis, fuzzystrmatch, postgis_tiger_geocoder, and tds_fdw.

--- src/functions/F_DEMO_ENROLL.sql
+++ src/functions/F_DEMO_ENROLL.sql
@@ -1,22 +1,30 @@
+
+-- Tests whether the function is currently built on the data base
 IF object_id(N'dbo.F_DEMO_ENROLL', N'TF') IS NOT NULL
+
+	-- If the function exists drop it before redefining it
     DROP FUNCTION dbo.F_DEMO_ENROLL;
 
+-- End of batch block
 GO
 
+-- Defines function to return demographics and enrollmnent data for 
+-- Students based on a user specified enrollment window
 CREATE FUNCTION dbo.F_DEMO_ENROLL(@start DATE, @end DATE)
 
+	-- Defines the table structure that will be returned by the function
 	RETURNS @retval TABLE (
 			schid VARCHAR(6) NOT NULL,
 			pid INT PRIMARY KEY,
-      schyr INT NOT NULL,
+			schyr INT NOT NULL,
 			sasid VARCHAR(15) NOT NULL,
 			firstnm VARCHAR(50) NOT NULL,
 			mi VARCHAR(1),
 			lastnm VARCHAR(50) NOT NULL,
 			dob DATE NOT NULL,
 			schnm VARCHAR(50) NOT NULL,
-      sdate DATE NOT NULL,
-      edate DATE,
+			sdate DATE NOT NULL,
+			edate DATE,
 			grade VARCHAR(2),
 			sex VARCHAR(6),
 			race VARCHAR(39),
@@ -26,96 +34,134 @@ CREATE FUNCTION dbo.F_DEMO_ENROLL(@start DATE, @end DATE)
 			frl VARCHAR(24),
 			tag VARCHAR(19),
 			gap VARCHAR(26),
-      hhm VARCHAR(23)
+			hhm VARCHAR(23),
+			migrant VARCHAR(25),
+			section504 VARCHAR(50),
+			immigrant VARCHAR(50),
+			refugee VARCHAR(50)
 		) AS
 
+	-- Starts the body of the function
 	BEGIN
 
+		-- Insert the data into the table variable that will be returned by this function
 		INSERT @retval (schid, pid, schyr, sasid, firstnm, mi, lastnm, dob, schnm, sdate,
-										edate, grade, sex, race, swd, ell, usentry, frl, tag, gap, hhm)
+						edate, grade, sex, race, swd, ell, usentry, frl, tag, gap, hhm, 
+						migrant, section504, immigrant, refugee)
+
+		-- Selects the data that will be inserted into the table variable 
 		SELECT b.schid, b.pid, b.schyr, b.sasid, b.firstnm, b.mi, b.lastnm, b.dob, b.schnm,
 					 b.sdate, b.edate, b.grade, b.sex, b.race, b.swd, b.ell, b.usentry, b.frl, b.tag,
-					 b.gap, b.hhm
+					 b.gap, b.hhm, b.migrant, b.section504, b.immigrant, b.refugee
+
+		-- The data are selected from a subquery so the gap group identifier can be defined			 
 		FROM (
+
+			-- Defines the query used to build the table
 			SELECT 	a.*,
+
+					-- Business logic for the gap group
 					CASE
 						WHEN a.race IN ('White (Non-Hispanic)', 'Native Hawaiian or Other Pacific Island', 'Asian') AND
 								 a.swd IS NULL AND a.ell IS NULL AND a.frl = 'Full-Price Meals'
-						THEN NULL
+						THEN 'Not in Gap Group'
 						ELSE 'Gap Group (non-duplicated)'
 					END AS gap
-			FROM (SELECT DISTINCT '165' + LTRIM(RTRIM(s.number)) AS schid,
-							p.personid AS pid,
-              FCPS_BB.dbo.F_ENDYEAR(y.sdate, DEFAULT) AS schyr,
-							p.stateID AS sasid,
-							i.firstName AS firstnm,
-							SUBSTRING(i.middleName, 1, 1) AS mi,
-							i.lastName AS lastnm,
-							CAST(i.birthdate AS DATE) AS dob,
-							s.name AS schnm,
-              CAST(y.sdate AS DATE) AS sdate,
-              CAST(y.edate AS DATE) AS edate,
-							e.grade,
-							CASE
-								WHEN i.gender = 'M' THEN 'Male'
-								WHEN i.gender = 'F' THEN 'Female'
-								ELSE NULL
-							END                          AS sex,
-							CASE
-								WHEN i.raceEthnicityFed = 1 THEN 'Hispanic'
-								WHEN i.raceEthnicityFed = 2 THEN 'American Indian or Alaska Native'
-								WHEN i.raceEthnicityFed = 3 THEN 'Asian'
-								WHEN i.raceEthnicityFed = 4 THEN 'African American'
-								WHEN i.raceEthnicityFed = 5 THEN 'Native Hawaiian or Other Pacific Island'
-								WHEN i.raceEthnicityFed = 6 THEN 'White (Non-Hispanic)'
-								ELSE 'Two or more races'
-							END AS race,
-							CASE
-								WHEN (e.specialEdStatus IN ('A', 'AR') OR
-									 (e.specialEdStatus = 'I' AND e.spedExitDate BETWEEN e.startDate AND e.endDate))
-								THEN 'Disability-With IEP (Total)'
-								ELSE NULL
-							END AS swd,
-							CASE
-								WHEN l.lepID IS NOT NULL THEN 'English Learner'
-								ELSE NULL
-							END AS ell,
-							CAST(i.dateEnteredUS AS DATE) AS usentry,
-							CASE
-								WHEN pe.eligibility IS NOT NULL THEN 'Free/Reduced-Price Meals'
-								ELSE 'Full-Price Meals'
-							END AS frl,
-							CASE
-								WHEN g.giftedID IS NOT NULL AND g.category = '12' THEN 'Primary Talent Pool'
-								WHEN g.giftedID IS NOT NULL THEN 'Gifted/Talented'
-								ELSE NULL
-							END AS tag,
-              CASE
-                WHEN e.homeless = 1 THEN 'Homeless/Highly Mobile'
-                ELSE 'Stable Housing'
-              END AS hhm
-				FROM 					[fayette].[dbo].[Person] p WITH ( NOLOCK )
-				INNER JOIN 		[fayette].[dbo].[Identity] i WITH ( NOLOCK )	ON 	i.identityID = p.currentIdentityID
-				INNER JOIN 		[fayette].[dbo].[Enrollment] e WITH ( NOLOCK )	ON 	p.personID = e.personID AND
+
+			-- This is the query where the demographics and all that are selected		
+			FROM (SELECT DISTINCT 	'165' + LTRIM(RTRIM(s.number)) AS schid,
+									p.personid AS pid,
+              						FCPS_BB.dbo.F_ENDYEAR(y.sdate, DEFAULT) AS schyr,
+									p.stateID AS sasid,
+									i.firstName AS firstnm,
+									SUBSTRING(i.middleName, 1, 1) AS mi,
+									i.lastName AS lastnm,
+									CAST(i.birthdate AS DATE) AS dob,
+									s.name AS schnm,
+						            CAST(y.sdate AS DATE) AS sdate,
+						            CAST(y.edate AS DATE) AS edate,
+									e.grade,
+									CASE
+										WHEN i.gender = 'M' THEN 'Male'
+										WHEN i.gender = 'F' THEN 'Female'
+										ELSE NULL
+									END                          AS sex,
+									CASE
+										WHEN i.raceEthnicityFed = 1 THEN 'Hispanic'
+										WHEN i.raceEthnicityFed = 2 THEN 'American Indian or Alaska Native'
+										WHEN i.raceEthnicityFed = 3 THEN 'Asian'
+										WHEN i.raceEthnicityFed = 4 THEN 'African American'
+										WHEN i.raceEthnicityFed = 5 THEN 'Native Hawaiian or Other Pacific Island'
+										WHEN i.raceEthnicityFed = 6 THEN 'White (Non-Hispanic)'
+										ELSE 'Two or more races'
+									END AS race,
+									CASE
+										WHEN (e.specialEdStatus IN ('A', 'AR') OR
+											 (e.specialEdStatus = 'I' AND e.spedExitDate BETWEEN e.startDate AND e.endDate))
+										THEN 'Disability-With IEP (Total)'
+										ELSE NULL
+									END AS swd,
+									CASE
+										WHEN l.lepID IS NOT NULL THEN 'English Learner'
+										ELSE NULL
+									END AS ell,
+									CAST(i.dateEnteredUS AS DATE) AS usentry,
+									CASE
+										WHEN pe.eligibility IS NOT NULL THEN 'Free/Reduced-Price Meals'
+										ELSE 'Full-Price Meals'
+									END AS frl,
+									CASE
+										WHEN g.giftedID IS NOT NULL AND g.category = '12' THEN 'Primary Talent Pool'
+										WHEN g.giftedID IS NOT NULL THEN 'Gifted/Talented'
+										ELSE NULL
+									END AS tag,
+						            CASE
+						                WHEN e.homeless = 1 THEN 'Homeless/Highly Mobile'
+						                ELSE 'Stable Housing'
+						            END AS hhm,
+						            CASE 
+						            	WHEN e.migrant = 2 THEN '2'
+						            	WHEN e.migrant = 1 THEN '1'
+						            	WHEN e.migrant = 0 THEN '0'
+						            	ELSE 'Missing Migrant Info'
+						            END AS migrant,
+						            CASE
+						            	WHEN e.section504 = 1 THEN '1'
+						            	WHEN e.section504 = 0 THEN '0'
+						            	ELSE 'Missing Section 504 Info'
+						            END AS section504,
+						            CASE
+						            	WHEN e.immigrant = 1 THEN '1'
+						            	WHEN e.immigrant = 0 THEN '0'
+						            	ELSE 'Missing Immigrant Info'
+						            END AS immigrant,
+						            CASE
+						            	WHEN e.refugee = 1 THEN '1'
+						            	WHEN e.refugee = 0 THEN '0'
+						            	ELSE 'Missing refugee Info'
+						            END AS refugee
+
+				FROM 				[fayette].[dbo].[Person] p WITH ( NOLOCK )
+				INNER JOIN 			[fayette].[dbo].[Identity] i WITH ( NOLOCK )	ON 	i.identityID = p.currentIdentityID
+				INNER JOIN 			[fayette].[dbo].[Enrollment] e WITH ( NOLOCK )	ON 	p.personID = e.personID AND
 																						ISNULL(e.noShow, 0) = 0 AND
 																						ISNULL(e.stateExclude, 0) = 0 AND
 																						e.serviceType = 'p'
-				INNER JOIN 		[fayette].[dbo].[Calendar] c WITH ( NOLOCK ) 		ON 	e.calendarID = c.calendarID
+				INNER JOIN 			[fayette].[dbo].[Calendar] c WITH ( NOLOCK ) 	ON 	e.calendarID = c.calendarID
+				INNER JOIN			[fayette].[dbo].[EnrollmentKY] eky WITH ( NOLOCK ) ON 	e.personID = eky.personID AND 
+																							c.calendarID = eky.calendarID AND 
+																							e.enrollmentID = eky.enrollmentID
+		        --Max Enrollment for School
+		        INNER JOIN (		SELECT 	e.personID, MAX(e.startDate) AS sdate,
+			                  					MAX(NULLIF(e.endDate, @end)) AS edate
+			              			FROM 	[fayette].[dbo].[Enrollment] e WITH ( NOLOCK )
+			              			WHERE 	ISNULL(e.noShow, 0) = 0 AND ISNULL(e.stateExclude, 0) = 0 AND
+			                  				e.serviceType = 'p' AND e.startDate <= @end AND
+			                  				ISNULL(e.endDate, @end) >= @start AND
+			                  				e.endYear = FCPS_BB.dbo.F_ENDYEAR(@end, DEFAULT)
+			              			GROUP BY e.personID) y ON e.personID = y.personID AND e.startDate = y.sdate
 
-        --Max Enrollment for School
-        INNER JOIN (
-              SELECT 	e.personID,
-                  		MAX(e.startDate) AS sdate,
-                  		MAX(NULLIF(e.endDate, @end)) AS edate
-              FROM 	  [fayette].[dbo].[Enrollment] e WITH ( NOLOCK )
-              WHERE 	ISNULL(e.noShow, 0) = 0 AND ISNULL(e.stateExclude, 0) = 0 AND
-                  		e.serviceType = 'p' AND e.startDate <= @end AND
-                  		ISNULL(e.endDate, @end) >= @start AND
-                  		e.endYear = FCPS_BB.dbo.F_ENDYEAR(@end, DEFAULT)
-              GROUP BY e.personID
-          ) y ON e.personID = y.personID AND e.startDate = y.sdate
-
-				INNER JOIN 		[fayette].[dbo].[School] s WITH ( NOLOCK ) 			ON 	c.schoolID = s.schoolID
+				INNER JOIN 			[fayette].[dbo].[School] s WITH ( NOLOCK ) 		ON 	c.schoolID = s.schoolID
 
 				--Gets English Language Learner Indicator
 				LEFT JOIN 		[fayette].[dbo].[Lep] l WITH ( NOLOCK ) 			ON 	p.personID = l.personID AND
@@ -127,26 +173,32 @@ CREATE FUNCTION dbo.F_DEMO_ENROLL(@start DATE, @end DATE)
 																					l.exitDate > e.endDate)))
 
 				--Free/Reduced Price Lunch Indicator
-				LEFT JOIN 	[fayette].[dbo].[POSEligibility] pe WITH (NOLOCK) 	ON 	p.personID = pe.personID AND
-																				e.endYear = pe.endYear AND
-																				pe.eligibility IN ('F', 'R')
+				LEFT JOIN 		[fayette].[dbo].[POSEligibility] pe WITH (NOLOCK) 	ON 	p.personID = pe.personID AND
+																						e.endYear = pe.endYear AND
+																						pe.eligibility IN ('F', 'R')
 
 				/*FRED at the beginning of the year is very tricky due to files not being loaded
 				until after the year starts and not being able to backdate some things - Dana has more
 				info. */
 				--GT --category 12 is Primary Talent Pool - Decide to include or not depending on need
-				LEFT OUTER JOIN [fayette].[dbo].[GiftedStatusKY] g 				ON 	g.personID = p.personID AND
-														 						g.endDate IS NULL
+				LEFT OUTER JOIN [fayette].[dbo].[GiftedStatusKY] g 					ON 	g.personID = p.personID AND
+														 								g.endDate IS NULL
 
 				WHERE 	e.endYear = FCPS_BB.dbo.F_ENDYEAR(@end, DEFAULT) AND
-								e.grade IN (00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 14) AND
-								e.startDate <= @end AND ISNULL(e.endDate, @end) BETWEEN @start AND @end
+						e.grade IN (00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 14) AND
+						e.startDate <= @end AND ISNULL(e.endDate, @end) >= @start
 				) AS a
+			
+			-- End of the definition of the subquery from which the table variable is populated
 			) AS b;
 
+		-- Return the table variable 
 		RETURN;
 
+	-- End of the function body
 	END
+
+-- End of the batch block	
 GO
 
 -- Executes an example query to use for testing purposes

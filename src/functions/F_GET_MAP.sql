71e3ab240fcc55c2dd231265109ac9da0d2cac61
wbuchanan <william@williambuchanan.net>
2016-11-22 07:19:31 -0500

    Checked function again.  Now executes in < 2 minutes.  Assumes that the highest scale score and percentile are the true scores for cases where a student has multiple score instanes in the same period/school year.  This prevents a violation of the primary key constraint on the pid column.

--- src/functions/F_GET_MAP.sql
+++ src/functions/F_GET_MAP.sql
@@ -34,86 +34,157 @@ CREATE FUNCTION dbo.F_GET_MAP(@schyr SMALLINT, @persid INT = NULL)
     IF @persid IS NULL
 
       -- Use a common table expression to handle the correlated subqueries
-      WITH a AS ( SELECT DISTINCT   ts.personID AS pid,
-                                    CAST(ts.scaleScore AS SMALLINT) AS sc,
-                                    ts.testID AS tid,
-                                    CAST(ts.percentile AS TINYINT) AS pct,
-                                    dbo.F_MAP_PERIODS(ts.date) AS period
-                  FROM              [fayette].[dbo].[TestScore] AS ts
-                  WHERE             ts.testID BETWEEN 1427 AND 1429 AND
-                                    dbo.F_ENDYEAR(ts.date, DEFAULT) = @schyr)
+      WITH a AS ( SELECT DISTINCT   a.pid, a.tid, a.period, MAX(a.pct) AS pct, MAX(a.sc) AS sc
+                  FROM (     SELECT DISTINCT  ts.personID AS pid,
+                                              ts.testID AS tid,
+                                              dbo.F_MAP_PERIODS(ts.date) AS period,
+                                              CAST(ts.percentile AS TINYINT) AS pct,
+                                              CAST(ts.scaleScore AS SMALLINT) AS sc
+                              FROM            [fayette].[dbo].[TestScore] AS ts
+                              WHERE           ts.testID BETWEEN 1427 AND 1429 AND
+                                              dbo.F_ENDYEAR(ts.date, DEFAULT) = @schyr AND
+                                              (ts.scaleScore IS NOT NULL OR ts.percentile IS NOT NULL)) AS a
+                  GROUP BY          a.pid, a.tid, a.period)
 
       -- Put the data into the
       INSERT @retval(pid, schyr,
                     mapmthsc1, mapmthpct1, maprlasc1, maprlapct1, maplansc1, maplanpct1,
                     mapmthsc2, mapmthpct2, maprlasc2, maprlapct2, maplansc2, maplanpct2,
                     mapmthsc3, mapmthpct3, maprlasc3, maprlapct3, maplansc3, maplanpct3)
-      SELECT DISTINCT   a.pid, @schyr AS schyr,
-                        CASE WHEN b.period = 1 THEN b.sc END AS mapmthsc1,
-                        CASE WHEN b.period = 1 THEN b.pct END AS mapmthpct1,
-                        CASE WHEN c.period = 1 THEN c.sc END AS maprlasc1,
-                        CASE WHEN c.period = 1 THEN c.pct END AS maprlapct1,
-                        CASE WHEN d.period = 1 THEN d.sc END AS maplansc1,
-                        CASE WHEN d.period = 1 THEN d.pct END AS maplanpct1,
-                        CASE WHEN b.period = 2 THEN b.sc END AS mapmthsc2,
-                        CASE WHEN b.period = 2 THEN b.pct END AS mapmthpct2,
-                        CASE WHEN c.period = 2 THEN c.sc END AS maprlasc2,
-                        CASE WHEN c.period = 2 THEN c.pct END AS maprlapct2,
-                        CASE WHEN d.period = 2 THEN d.sc END AS maplansc2,
-                        CASE WHEN d.period = 2 THEN d.pct END AS maplanpct2,
-                        CASE WHEN b.period = 3 THEN b.sc END AS mapmthsc3,
-                        CASE WHEN b.period = 3 THEN b.pct END AS mapmthpct3,
-                        CASE WHEN c.period = 3 THEN c.sc END AS maprlasc3,
-                        CASE WHEN c.period = 3 THEN c.pct END AS maprlapct3,
-                        CASE WHEN d.period = 3 THEN d.sc END AS maplansc3,
-                        CASE WHEN d.period = 3 THEN d.pct END AS maplanpct3
+      SELECT DISTINCT   a2.pid, @schyr AS schyr,
+                        b.sc AS mapmthsc1,
+                        b.pct AS mapmthpct1,
+                        c.sc AS maprlasc1,
+                        c.pct AS maprlapct1,
+                        d.sc AS maplansc1,
+                        d.pct AS maplanpct1,
+                        e.sc AS mapmthsc2,
+                        e.pct AS mapmthpct2,
+                        f.sc AS maprlasc2,
+                        f.pct AS maprlapct2,
+                        g.sc AS maplansc2,
+                        g.pct AS maplanpct2,
+                        h.sc AS mapmthsc3,
+                        h.pct AS mapmthpct3,
+                        i.sc AS maprlasc3,
+                        i.pct AS maprlapct3,
+                        j.sc AS maplansc3,
+                        j.pct AS maplanpct3
       FROM              a
-      LEFT JOIN         a AS b ON a.pid = b.pid AND b.tid = 1427
-      LEFT JOIN         a AS c ON a.pid = c.pid AND c.tid = 1428
-      LEFT JOIN         a AS d ON a.pid = d.pid AND d.tid = 1429
 
+      -- Fall Math Scores
+      LEFT JOIN   		a AS b ON a.pid = b.pid AND b.tid = 1427 AND b.period = 1 AND
+                                  (b.sc IS NOT NULL OR b.pct IS NOT NULL)
+
+      -- Fall Reading/Language Arts Scores
+      LEFT JOIN   		a AS c ON a.pid = c.pid AND c.tid = 1428 AND c.period = 1 AND
+                                  (c.sc IS NOT NULL OR c.pct IS NOT NULL)
+
+      -- Fall Language Usage Scores
+      LEFT JOIN   		a AS d ON a.pid = d.pid AND d.tid = 1429 AND d.period = 1 AND
+                                  (d.sc IS NOT NULL OR d.pct IS NOT NULL)
+
+      -- Winter Math Scores
+      LEFT JOIN   		a AS e ON a.pid = e.pid AND e.tid = 1427 AND e.period = 2 AND
+                                  (e.sc IS NOT NULL OR e.pct IS NOT NULL)
+
+      -- Winter Reading/Language Arts Scores
+      LEFT JOIN   		a AS f ON a.pid = f.pid AND f.tid = 1428 AND f.period = 2 AND
+                                  (f.sc IS NOT NULL OR f.pct IS NOT NULL)
+
+      -- Winter Language Usage Scores
+      LEFT JOIN   		a AS g ON a.pid = g.pid AND g.tid = 1429 AND g.period = 2 AND
+                                  (g.sc IS NOT NULL OR g.pct IS NOT NULL)
+
+      -- Spring Math Scores
+      LEFT JOIN   		a AS h ON a.pid = h.pid AND h.tid = 1427 AND h.period = 3 AND
+                                  (h.sc IS NOT NULL OR h.pct IS NOT NULL)
+
+      -- Spring Reading/Language Arts Scores
+      LEFT JOIN   		a AS i ON a.pid = i.pid AND i.tid = 1428 AND i.period = 3 AND
+                                  (i.sc IS NOT NULL OR i.pct IS NOT NULL)
+
+      -- Spring Language Usage Scores
+      LEFT JOIN   		a AS j ON a.pid = j.pid AND j.tid = 1429 AND j.period = 3 AND
+                                  (j.sc IS NOT NULL OR j.pct IS NOT NULL);
 
     -- If a person ID is passed to the function
     ELSE
 
       -- Use a common table expression to handle the correlated subqueries
-      WITH a AS ( SELECT DISTINCT   ts.personID AS pid,
-                                    CAST(ts.scaleScore AS SMALLINT) AS sc,
-                                    ts.testID AS tid,
-                                    CAST(ts.percentile AS TINYINT) AS pct,
-                                    dbo.F_MAP_PERIODS(ts.date) AS period
-                  FROM              [fayette].[dbo].[TestScore] AS ts
-                  WHERE             ts.testID BETWEEN 1427 AND 1429 AND
-                                    dbo.F_ENDYEAR(ts.date, DEFAULT) = @schyr AND
-                                    ts.personID = @persid)
+      WITH a AS ( SELECT DISTINCT   a.pid, a.tid, a.period, MAX(a.pct) AS pct, MAX(a.sc) AS sc
+                  FROM (     SELECT DISTINCT  ts.personID AS pid,
+                                              ts.testID AS tid,
+                                              dbo.F_MAP_PERIODS(ts.date) AS period,
+                                              CAST(ts.percentile AS TINYINT) AS pct,
+                                              CAST(ts.scaleScore AS SMALLINT) AS sc
+                              FROM            [fayette].[dbo].[TestScore] AS ts
+                              WHERE           ts.testID BETWEEN 1427 AND 1429 AND
+                                              dbo.F_ENDYEAR(ts.date, DEFAULT) = @schyr AND
+                                              ts.personID = @persid AND
+                                              (ts.scaleScore IS NOT NULL OR ts.percentile IS NOT NULL)) AS a
+                  GROUP BY          a.pid, a.tid, a.period)
       -- Put the data into the
       INSERT @retval(pid, schyr,
                     mapmthsc1, mapmthpct1, maprlasc1, maprlapct1, maplansc1, maplanpct1,
                     mapmthsc2, mapmthpct2, maprlasc2, maprlapct2, maplansc2, maplanpct2,
                     mapmthsc3, mapmthpct3, maprlasc3, maprlapct3, maplansc3, maplanpct3)
       SELECT DISTINCT   a.pid, @schyr AS schyr,
-                        CASE WHEN b.period = 1 THEN b.sc END AS mapmthsc1,
-                        CASE WHEN b.period = 1 THEN b.pct END AS mapmthpct1,
-                        CASE WHEN c.period = 1 THEN c.sc END AS maprlasc1,
-                        CASE WHEN c.period = 1 THEN c.pct END AS maprlapct1,
-                        CASE WHEN d.period = 1 THEN d.sc END AS maplansc1,
-                        CASE WHEN d.period = 1 THEN d.pct END AS maplanpct1,
-                        CASE WHEN b.period = 2 THEN b.sc END AS mapmthsc2,
-                        CASE WHEN b.period = 2 THEN b.pct END AS mapmthpct2,
-                        CASE WHEN c.period = 2 THEN c.sc END AS maprlasc2,
-                        CASE WHEN c.period = 2 THEN c.pct END AS maprlapct2,
-                        CASE WHEN d.period = 2 THEN d.sc END AS maplansc2,
-                        CASE WHEN d.period = 2 THEN d.pct END AS maplanpct2,
-                        CASE WHEN b.period = 3 THEN b.sc END AS mapmthsc3,
-                        CASE WHEN b.period = 3 THEN b.pct END AS mapmthpct3,
-                        CASE WHEN c.period = 3 THEN c.sc END AS maprlasc3,
-                        CASE WHEN c.period = 3 THEN c.pct END AS maprlapct3,
-                        CASE WHEN d.period = 3 THEN d.sc END AS maplansc3,
-                        CASE WHEN d.period = 3 THEN d.pct END AS maplanpct3
+                        b.sc AS mapmthsc1,
+                        b.pct AS mapmthpct1,
+                        c.sc AS maprlasc1,
+                        c.pct AS maprlapct1,
+                        d.sc AS maplansc1,
+                        d.pct AS maplanpct1,
+                        e.sc AS mapmthsc2,
+                        e.pct AS mapmthpct2,
+                        f.sc AS maprlasc2,
+                        f.pct AS maprlapct2,
+                        g.sc AS maplansc2,
+                        g.pct AS maplanpct2,
+                        h.sc AS mapmthsc3,
+                        h.pct AS mapmthpct3,
+                        i.sc AS maprlasc3,
+                        i.pct AS maprlapct3,
+                        j.sc AS maplansc3,
+                        j.pct AS maplanpct3
       FROM              a
-      LEFT JOIN         a AS b ON a.pid = b.pid AND b.tid = 1427
-      LEFT JOIN         a AS c ON a.pid = c.pid AND c.tid = 1428
-      LEFT JOIN         a AS d ON a.pid = d.pid AND d.tid = 1429;
+
+      -- Fall Math Scores
+      LEFT JOIN   a AS b ON a.pid = b.pid AND b.tid = 1427 AND b.period = 1 AND
+                                  (b.sc IS NOT NULL OR b.pct IS NOT NULL)
+
+      -- Fall Reading/Language Arts Scores
+      LEFT JOIN   a AS c ON a.pid = c.pid AND c.tid = 1428 AND c.period = 1 AND
+                                  (c.sc IS NOT NULL OR c.pct IS NOT NULL)
+
+      -- Fall Language Usage Scores
+      LEFT JOIN   a AS d ON a.pid = d.pid AND d.tid = 1429 AND d.period = 1 AND
+                                  (d.sc IS NOT NULL OR d.pct IS NOT NULL)
+
+      -- Winter Math Scores
+      LEFT JOIN   a AS e ON a.pid = e.pid AND e.tid = 1427 AND e.period = 2 AND
+                                  (e.sc IS NOT NULL OR e.pct IS NOT NULL)
+
+      -- Winter Reading/Language Arts Scores
+      LEFT JOIN   a AS f ON a.pid = f.pid AND f.tid = 1428 AND f.period = 2 AND
+                                  (f.sc IS NOT NULL OR f.pct IS NOT NULL)
+
+      -- Winter Language Usage Scores
+      LEFT JOIN   a AS g ON a.pid = g.pid AND g.tid = 1429 AND g.period = 2 AND
+                                  (g.sc IS NOT NULL OR g.pct IS NOT NULL)
+
+      -- Spring Math Scores
+      LEFT JOIN   a AS h ON a.pid = h.pid AND h.tid = 1427 AND h.period = 3 AND
+                                  (h.sc IS NOT NULL OR h.pct IS NOT NULL)
+
+      -- Spring Reading/Language Arts Scores
+      LEFT JOIN   a AS i ON a.pid = i.pid AND i.tid = 1428 AND i.period = 3 AND
+                                  (i.sc IS NOT NULL OR i.pct IS NOT NULL)
+
+      -- Spring Language Usage Scores
+      LEFT JOIN   a AS j ON a.pid = j.pid AND j.tid = 1429 AND j.period = 3 AND
+                                  (j.sc IS NOT NULL OR j.pct IS NOT NULL);
 
   -- Returns the table valued return object
   RETURN;
@@ -126,8 +197,5 @@ GO
 
 -- Example of using the function to retrieve all records for the 2016-2017 school year
 SELECT *
-FROM FCPS_BB.dbo.F_GET_MAP(2017, DEFAULT);
+FROM FCPS_BB.dbo.F_GET_MAP(2015, DEFAULT);
 
--- Example of using the function to retrieve records for the student with personID 10 for the 2016-2017 school year
-SELECT *
-FROM FCPS_BB.dbo.F_GET_MAP(2017, 10);
